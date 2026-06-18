ce66ea04aa0853bd33287a7e5c97b5d6e121129f
suren-hov <shovhannisyan2021@gmail.com>
2026-06-18 16:39:55 +0400

    Added output directory

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use('/api/stats', statsRoutes);
 
 app.get('/', (req, res) => {
-  res.send('AffPapa La Liga Stats API. Try GET /api/stats/la-liga-2024?format=excel');
+  res.send('La Liga Stats API. Try GET /api/stats/la-liga-2024?format=excel');
 });
 
 app.listen(PORT, () => {
